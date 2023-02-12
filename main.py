503d783f5e0fb5b4d69e488d15124605fa8bcead
Pedro Lemos <lemozxpedro@gmail.com>
2023-02-12 19:55:13 -0300

    Migrated to Python, adjusted delay

---
+++ main.py
@@ -0,0 +1,18 @@
+from bad_apple import frames
+from playsound import playsound
+import os
+import time
+import threading
+
+audio_file = os.path.join(os.getcwd(), 'assets', 'bad_apple.wav')
+delay = 0.01588  # 1000 / 60 -> 16.66 ms -> 0.01666 sec -> 0.01588 sec (sync)
+
+threading.Thread(
+    target=playsound,
+    args=(audio_file,),
+    daemon=True
+).start()
+
+for frame in frames:
+    print(frame, end='')
+    time.sleep(delay)
